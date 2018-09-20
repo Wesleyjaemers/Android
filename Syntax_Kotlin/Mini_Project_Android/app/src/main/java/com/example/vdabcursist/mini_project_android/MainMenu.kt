aedf00ee20384797c1dda634dc11e93eb861ea5e
Wesleyjaemers <jaemers.wesley@gmail.com>
2018-09-20 12:40:27 +0200

    update miniproject2

--- Syntax_Kotlin/Mini_Project_Android/app/src/main/java/com/example/vdabcursist/mini_project_android/MainMenu.kt
+++ Syntax_Kotlin/Mini_Project_Android/app/src/main/java/com/example/vdabcursist/mini_project_android/MainMenu.kt
@@ -3,7 +3,9 @@ package com.example.vdabcursist.mini_project_android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
-import android.widget.LinearLayout
+import android.view.View
+import android.text.method.PasswordTransformationMethod
+
 
 
 
@@ -14,6 +16,8 @@ class MainMenu : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val constraintLayout = findViewById(R.id.constraint_layout_ID) as ConstraintLayout
-        constraintLayout.setBackgroundResource(R.drawable.BackgroundMain)
+        constraintLayout.setBackgroundResource(R.drawable.background_main)
     }
+
 }
+
